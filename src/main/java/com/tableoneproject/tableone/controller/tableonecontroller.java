9f66b05b09f13fadb892601703811e1a1e8a0d9a
lakshmi-dasari <lakshmi.dasari1992@gmail.com>
2019-06-09 00:24:59 -0400

    added crossorigion

--- src/main/java/com/tableoneproject/tableone/controller/tableonecontroller.java
+++ src/main/java/com/tableoneproject/tableone/controller/tableonecontroller.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,6 +22,7 @@ public class tableonecontroller {
 	@Autowired
 	private tableonerepo trepo;
 	
+	@CrossOrigin(origins = "http://localhost:8888")
 	@GetMapping("/getmethod")
 	public List<tableonemodel> methodname(){
 		return trepo.findAll();
@@ -30,4 +32,5 @@ public class tableonecontroller {
 	public tableonemodel methodname(@Valid @RequestBody tableonemodel tmodel) {
 		return trepo.save(tmodel);
 	}
+	
 }
